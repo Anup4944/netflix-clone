30c9bbfe97fdf9dbb2109b65b63f101939c3b583
Anup <anuppoudel60@gmail.com>
2022-06-28 17:52:17 +1000

    home created

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,74 @@
 import React from "react";
 import "./Home.scss";
 
+const Card = ({ img }) => {
+  return <img src={img} alt="cover" className="card" />;
+};
+
+const Row = ({ title }) => (
+  <div className="row">
+    <h2>{title}</h2>
+    <div>
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+      <Card
+        img={
+          "https://www.thestreambible.com/wp-content/uploads/2022/02/peaky-F-758x405.jpg"
+        }
+      />
+    </div>
+  </div>
+);
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="banner"></div>
+      <Row title={"Popular on Netflix"} />
+    </section>
+  );
 };
 
 export default Home;
